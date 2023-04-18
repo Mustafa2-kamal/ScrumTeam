44317e8f12f45d16bcad887b0cecc27f585eb9f3
seba-salahat5 <80277805+seba-salahat5@users.noreply.github.com>
2023-04-18 05:48:15 +0300

    Load Countries Function

---
+++ src/utils/Api_Functions.js
@@ -0,0 +1,20 @@
+export async function loadCountries(searchTerm) {
+    let url = ((searchTerm === '') || (!searchTerm))
+        ? `https://restcountries.com/v3.1/all`
+        : `https://restcountries.com/v3.1/name/${searchTerm}`;
+    return await fetch(url)
+        .then(response => {
+            switch (response.status) {
+                case 404:
+                    return [];
+                case 200:
+                    return response.json();
+                default:
+                    return null;
+            }
+        })
+        .catch(error => console.log(error))
+        .then(data => {
+            return data;
+        });
+}
