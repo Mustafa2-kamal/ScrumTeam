cd6245591fc7bbdcd4a9537637310ef115f6a554
AmraboAmr <124199949+AmraboAmr@users.noreply.github.com>
2023-04-19 23:32:33 +0300

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import DropMenu from '../components/DropMenu';
+
 
 function HomePage() {
     return (
         <>
             <h1>Inside Home Page</h1>
-            <DropMenu/>
+         
         </>
     );
 }
