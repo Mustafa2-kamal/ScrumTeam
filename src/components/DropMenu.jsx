5922a49c0376efcee662d3d872483df975f826f7
Mustafa2-kamal <mostafakamalsokar@gmail.com>
2023-04-19 23:11:34 +0300

    dropMenu component

--- src/components/DropMenu.jsx
+++ src/components/DropMenu.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from 'styled-components';
 
 const StyledDropDown = styled.select`
-    width: 50%;
-    padding: 20px ;
+    width: 100%;
+    padding: 15px ;
     border: none;
     border-radius: 5px;
     font-size: 15px;
@@ -13,27 +13,26 @@ const StyledDropDown = styled.select`
     option {
         color: black;
         background: white;
-      }
+    }
 `;
 
 
-export default function DropMenu({filterValue,setFilterValue}) {
-   
+export default function DropMenu({ options, filterValue, setFilterValue }) {
+
     const handleFilterSelect = e => {
         setFilterValue(e.value);
     };
 
+
     return (
         <div>
             <StyledDropDown onChange={handleFilterSelect} value={filterValue}>
-                <option value="">Filter by</option>
-                <option value="">No Filter</option>
-                <option value="Favourites">Favourites</option>
-                <option value="Africa">Africa</option>
-                <option value="America">America</option>
-                <option value="Asia">Asia</option>
-                <option value="Europe">Europe</option>
-                <option value="Oceania">Oceania</option>
+                {
+                    options.map((option) => {
+                        return <option value={option}>{option}</option>
+
+                    })
+                }
             </StyledDropDown>
         </div>
     );
