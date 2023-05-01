d78d381e5b96b56ea690312a1689d893b4a67f73
MarahShabello <s11819990@stu.najah.edu>
2023-05-01 12:30:36 +0300

    add CountryText to the details page

--- src/pages/details.js
+++ src/pages/details.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { loadCountryDetails } from '../utils/Api_Functions';
+import CountryText from '../components/CountryText';
 
 function DetailsPage() {
-    return (
-        <>
-            <h1>Inside Details Page</h1>
-        </>
-    );
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const countryCode = params.get("countryCode");
+
+    const [countryInfo, setCountry] = useState([]);
+    const [borders, setBorders] = useState([]);
+
+    async function fetchCountryDetails() {
+        setCountry(await loadCountryDetails(countryCode));
+    }
+
+    function fetchBorders(borders) {
+        if (borders) {
+            const promises = [];
+            for (let i = 0; i < borders.length; i++) {
+                const promise = loadCountryDetails(borders[i])
+                promises.push(promise);
+            }
+            Promise.all(promises)
+                .then(response => {
+                    return response;
+                })
+                .then(data => {
+                    setBorders(data);
+                })
+        }
+    }
+
+    useEffect(() => {
+        fetchCountryDetails();
+    }, []);
+
+    useEffect(() => {
+        if (countryInfo[0]) {
+            fetchBorders(countryInfo[0].borders);
+        }
+    }, [countryInfo])
+
+    if (countryInfo && countryInfo.length > 0) {
+        console.log(borders);
+        return (
+            <>
+                <h1>Inside Details Page</h1>
+                <CountryText country={countryInfo[0]} />
+            </>
+        );
+    }
 }
 
 export default DetailsPage;
